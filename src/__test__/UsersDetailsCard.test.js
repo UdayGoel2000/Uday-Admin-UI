b5967f6e530cfd4d1cf66a95456849f39d1e2119
Uday Goel <udaygoel2000@gmail.com>
2023-07-24 23:18:38 +0530

    commit new Changes

--- src/__test__/UsersDetailsCard.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import UsersDetailsCard from "../components/UsersDetailsCard";
-import axios from "axios";
-
-test("renders the component correctly", () => {
-  render(<UsersDetailsCard />);
-  const table = screen.getByRole("table");
-  expect(table).toBeInTheDocument();
-});
